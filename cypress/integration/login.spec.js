c88748f8d822044c29829495eb220d421c794e4f
Jeferson Inacio Ferreira <jeferson.ferreira@yssy.com.br>
2021-12-28 19:40:03 -0300

    refactor for covered 100%

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,11 +1,12 @@
 /* eslint-disable linebreak-style */
 it('successfully logs in', () => {
   cy.intercept('GET', '**/notes').as('getNotes')
+
   cy.login(
     Cypress.env('USER_EMAIL'),
-    Cypress.env('USER_PASSWORD')
+    Cypress.env('USER_PASSWORD'),
+    { cacheSession: false }
   )
-  // eslint-disable-next-line cypress/no-unnecessary-waiting
-  cy.wait(15000)
+
   cy.wait('@getNotes')
 })
